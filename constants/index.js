f5f800243f8d006e2b6af51dfcc811efc65757e3
Sergey Guchok <gck.sergey@gmail.com>
2023-02-21 00:02:54 +0300

    fix

--- constants/index.js
+++ constants/index.js
@@ -31,10 +31,10 @@ export const TYPE_SECONDARY = 'secondary';
 // footer
 export const FOOTER_BACKGROUND = '#F5F5F5';
 
-export const PROD_URL =
-  process.env.ENVIRONMENT === 'production' ? API_URL : 'http://localhost:3000';
-
 export const API_URL =
   process.env.ENVIRONMENT === 'production'
-    ? API_URL
+    ? 'https://thegamblr.com/api'
     : 'http://localhost:3000/api';
+
+export const PROD_URL =
+  process.env.ENVIRONMENT === 'production' ? API_URL : 'http://localhost:3000';
